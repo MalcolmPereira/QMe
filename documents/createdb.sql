69e4eb346709f69281d881c3e03d2760ea3eccb7
Malcolm Pereira <malcolm.j.pereira@gmail.com>
2015-05-02 17:43:10 -0400

    updates for schema changes

--- documents/createdb.sql
+++ documents/createdb.sql
@@ -63,20 +63,20 @@ CREATE TABLE IF NOT EXISTS QUESTION (
 ) COMMENT 'Question maintains questions that can be part of a fixed set quiz or appear by itself for chose category in game mode';
 
 DROP TABLE IF EXISTS ANSWER_REFERENCE_MEDIA;
-CREATE TABLE IF NOT EXISTS ANSWER_REFERENCE_MEDIA (
-  ANSWER_REF_MEDIA_ID INT NOT NULL AUTO_INCREMENT COMMENT 'Primary Key explanation reference id',
-  QUESTION_ID INT NOT NULL COMMENT 'Explanation reference belongs to a question, a question explanation can have zero or more references',
+CREATE  TABLE IF NOT EXISTS ANSWER_REFERENCE_MEDIA (
+  ANSWER_REF_MEDIA_ID INT(11) NOT NULL AUTO_INCREMENT COMMENT 'Primary Key explanation reference id' ,
+  QUESTION_ID INT(11) NOT NULL COMMENT 'Explanation reference belongs to a question, a question explanation can have zero or more references' ,
   REF_MEDIA_TYPE VARCHAR(50) NOT NULL COMMENT 'Reference type mime type - images, videos etc' ,
-  REF_MEDIA BLOB NULL COMMENT 'Reference media can be stored and save from server',
+  REF_MEDIA BLOB NULL DEFAULT NULL COMMENT 'Reference media can be stored and save from server' ,
   PRIMARY KEY (ANSWER_REF_MEDIA_ID) ,
   UNIQUE INDEX ANSWER_REF_MEDIA_ID_UNIQUE (ANSWER_REF_MEDIA_ID ASC) ,
   INDEX ANSWER_REF_MEDIA_QUESTION_ID_FK_INDEX (QUESTION_ID ASC) ,
   CONSTRAINT ANSWER_EXPLAIN_MEDIA_QUESTION_ID_FK
-  FOREIGN KEY (QUESTION_ID)
-  REFERENCES QUESTION (QUESTION_ID)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION
-) COMMENT 'Answer reference maintains answer explanations for the questions, basically will provide the users with the additonal reference material for the question';
+    FOREIGN KEY (QUESTION_ID)
+    REFERENCES QUESTION (QUESTION_ID)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+)COMMENT = 'Answer reference maintains answer explanations for the questions, basically will provide the users with the additonal reference material for the question';
 
 DROP TABLE IF EXISTS QUESTION_HIT;
 CREATE TABLE IF NOT EXISTS QUESTION_HIT (
@@ -109,20 +109,20 @@ CREATE TABLE IF NOT EXISTS ANSWER_OPTION (
 ) COMMENT 'Answer options that appear as single or multiselect items, these are shown for question. Questions may have one or more options. Option likes may not also apply for example if its right or wrong, options like will not apply and will always be 0. ';
 
 DROP TABLE IF EXISTS ANSWER_OPTION_MEDIA;
-CREATE TABLE IF NOT EXISTS ANSWER_OPTION_MEDIA (
-  OPTION_MEDIA_ID INT NOT NULL AUTO_INCREMENT COMMENT 'Primary Key option media',
-  OPTION_ID INT NOT NULL ,
-  OPTION_MEDIA_TYPE VARCHAR(50) NOT NULL COMMENT 'Answer option type mime type - images, videos etc',
-  OPTION_MEDIA BLOB NULL COMMENT 'Media data which save and served from server',
+CREATE  TABLE IF NOT EXISTS ANSWER_OPTION_MEDIA (
+  OPTION_MEDIA_ID INT(11) NOT NULL AUTO_INCREMENT COMMENT 'Primary Key option media' ,
+  OPTION_ID INT(11) NOT NULL ,
+  OPTION_MEDIA_TYPE VARCHAR(50) NOT NULL COMMENT 'Answer option type mime type - images, videos etc' ,
+  OPTION_MEDIA BLOB NULL DEFAULT NULL COMMENT 'Media data which save and served from server' ,
   PRIMARY KEY (OPTION_MEDIA_ID) ,
   UNIQUE INDEX OPTION_MEDIA_ID_UNIQUE (OPTION_MEDIA_ID ASC) ,
   INDEX OPTION_MEDIA_OPTION_ID_FK_INDEX (OPTION_ID ASC) ,
   CONSTRAINT OPTION_MEDIA_OPTION_ID_FK
-  FOREIGN KEY (OPTION_ID)
-  REFERENCES ANSWER_OPTION (OPTION_ID)
-  ON DELETE CASCADE
-  ON UPDATE NO ACTION
-) COMMENT 'Answer options media maintain answer reference media - images, videos';
+    FOREIGN KEY (OPTION_ID)
+    REFERENCES ANSWER_OPTION(OPTION_ID)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION
+)COMMENT = 'Answer options media maintain answer reference media - images, videos';
 
 DROP TABLE IF EXISTS QUIZ;
 CREATE TABLE IF NOT EXISTS QUIZ (
@@ -265,6 +265,27 @@ CREATE TABLE IF NOT EXISTS USER_QUIZ (
   ON UPDATE NO ACTION
 ) COMMENT 'User Quiz maintain quiz results when user attempts fixed quizs.';
 
+DROP TABLE IF EXISTS USER_QUIZ_GAME;
+CREATE  TABLE IF NOT EXISTS USER_QUIZ_GAME (
+  USER_ID INT(11) NOT NULL ,
+  CAT_ID INT(11) NOT NULL COMMENT 'Category for Quiz Game Mode' ,
+  START_DATE TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
+  END_DATE TIMESTAMP NULL DEFAULT NULL ,
+  USER_SCORE INT(11) NOT NULL DEFAULT '0' ,
+  QUIZ_GAME_TOKEN BIGINT NOT NULL COMMENT 'Quiz Game Generated token to compare results' ,
+  PRIMARY KEY (USER_ID, CAT_ID, QUIZ_GAME_TOKEN) ,
+  INDEX USER_QUIZ_GAME_USER_ID_FK_INDEX (USER_ID ASC) ,
+  INDEX USER_QUIZ_GAME_CAT_ID_FK_INDEX (CAT_ID ASC) ,
+  CONSTRAINT USER_QUIZ_GAME_USER_ID_FK
+    FOREIGN KEY (USER_ID)
+    REFERENCES USER (USER_ID),
+  CONSTRAINT USER_QUIZ_GAME_CAT_ID_FK
+    FOREIGN KEY (CAT_ID)
+    REFERENCES CATEGORY(CAT_ID)
+)
+COMMENT = 'User can play quiz in game mode where random questions will  be displayed to the user, game ends when user gets x answers wrong.';
+
+
 DROP TABLE IF EXISTS ROLE;
 CREATE TABLE IF NOT EXISTS ROLE (
   ROLE_ID INT NOT NULL AUTO_INCREMENT,
@@ -330,265 +351,4 @@ VALUES ('Movies', '0', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP, 1, 0);
 
 -- Add Questions/Answer Options and Answer Reference for Default Java Quiz
 
--- Question 1
-INSERT INTO QUESTION (CAT_ID, QUESTION_TEXT,QUESTION_ANSWER,QUESTION_LIKES,QUESTION_CREATE_DATE,QUESTION_CREATE_USER,QUESTION_UPDATE_DATE,QUESTION_UPDATE_USER)
-VALUES (
-1,
-'With Java Memory in mind, can pick up which is the correct definition for stack and heap.',
-'Stack is the memory allocation where methods and primitive variables live, these are generally short lived. Heap is where Objects live these generally live longer.
-
-Java Stack memory is used for execution of a thread. They contain method specific values that are short-lived and references to other objects in the heap that are getting referred from the method. Stack memory is always referenced in LIFO (Last-In-First-Out) order. Whenever a method is invoked, a new block is created in the stack memory for the method to hold local primitive values and reference to other objects in the method. As soon as method ends, the block becomes unused and become available for next method.Stack memory size is very less compared to Heap memory.
-
-Heap memory is used by java runtime to allocate memory to Objects and JRE classes. Whenever we create any object, it�s always created in the Heap space. Garbage Collection runs on the heap memory to free the memory used by objects that doesn�t have any reference. Any object created in the heap space has global access and can be referenced from anywhere of the application.
-',
-0,
-CURRENT_TIMESTAMP,
-1,
-CURRENT_TIMESTAMP,
-1
-);		
--- Question 1 Answer Options
-INSERT INTO ANSWER_OPTION (QUESTION_ID, OPTION_TEXT, ISCORRECT) 
-VALUES (1, 
-'Stack is the memory allocation where objects live, these are generally long lived. Heap is where primitive variables and method calls live these generally short lived.', 
-0
-);
-INSERT INTO ANSWER_OPTION (QUESTION_ID, OPTION_TEXT, ISCORRECT) 
-VALUES (1, 
-'Heap is the memory allocation where methods and primitive variables live, these are generally short lived. Stack is where Objects live these generally live longer.', 
-0
-);
-INSERT INTO ANSWER_OPTION (QUESTION_ID, OPTION_TEXT, ISCORRECT) 
-VALUES (1, 
-'Stack is the memory allocation where methods and primitive variables live, these are generally short lived. Heap is where Objects live these generally live longer.', 
-1
-);
-INSERT INTO ANSWER_OPTION (QUESTION_ID, OPTION_TEXT, ISCORRECT) 
-VALUES (1, 
-'Stack and Heap are the same just different nomerclature for Java Memory Model.', 
-0
-);
--- Question 1 Answer Reference Media
-INSERT INTO ANSWER_REFERENCE_MEDIA (QUESTION_ID, REF_MEDIA_TYPE, REF_MEDIA_LINK_URL,REF_MEDIA ) 
-VALUES (1,
-'image/jpg',
-'http://www.codeproject.com/KB/dotnet/6importentStepsDotNet/13.jpg',
-LOAD_FILE('/tmp/stackheap.jpg')
-);
-INSERT INTO ANSWER_REFERENCE_MEDIA (QUESTION_ID, REF_MEDIA_TYPE, REF_MEDIA_LINK_URL) 
-VALUES (1,
-'url',
-'http://www.journaldev.com/4098/java-heap-memory-vs-stack-memory-difference'
-);
-INSERT INTO ANSWER_REFERENCE_MEDIA (QUESTION_ID, REF_MEDIA_TYPE, REF_MEDIA_LINK_URL) 
-VALUES (1,
-'url',
-'http://www.seas.upenn.edu/~ese112/spring09/programming/lectures/references.pdf'
-);
-
-
--- Question 2
-INSERT INTO QUESTION (CAT_ID, QUESTION_TEXT,QUESTION_ANSWER,QUESTION_LIKES,QUESTION_CREATE_DATE,QUESTION_CREATE_USER,QUESTION_UPDATE_DATE,QUESTION_UPDATE_USER)
-VALUES (
-1,
-'Identify the parent class of all Java Objects and the number of constructors.',
-'java.lang.Object class is the root of the class hierarchy. Every class has Object as a superclass. All objects, including arrays, implement the methods of this class.	
-
-The Object Class has one constructor which is the default no argument constructor.
-',
-0,
-CURRENT_TIMESTAMP,
-1,
-CURRENT_TIMESTAMP,
-1
-);		
--- Question 2 Answer Options
-INSERT INTO ANSWER_OPTION (QUESTION_ID, OPTION_TEXT, ISCORRECT) 
-VALUES (2, 
-'java.lang.Class is the root of the class hierarchy. java.lang.class has one default constructor.', 
-0
-);
-INSERT INTO ANSWER_OPTION (QUESTION_ID, OPTION_TEXT, ISCORRECT) 
-VALUES (2, 
-'java.lang.AbstractClass is the root of the class hierarchy. java.lang.class has one default constructor.', 
-0
-);
-INSERT INTO ANSWER_OPTION (QUESTION_ID, OPTION_TEXT, ISCORRECT) 
-VALUES (2, 
-'java.lang.Object class is the root of the class hierarchy. java.lang.Object has four different constructors. ', 
-0
-);
-INSERT INTO ANSWER_OPTION (QUESTION_ID, OPTION_TEXT, ISCORRECT) 
-VALUES (2, 
-'java.lang.Object class is the root of the class hierarchy. java.lang.Object has one default constructor. ', 
-1
-);
--- Question 2 Answer Reference Media
-INSERT INTO ANSWER_REFERENCE_MEDIA (QUESTION_ID, REF_MEDIA_TYPE, REF_MEDIA_LINK_URL) 
-VALUES (2,
-'url',
-'https://docs.oracle.com/javase/6/docs/api/java/lang/Object.html'
-);
-
--- Question 3
-INSERT INTO QUESTION (CAT_ID, QUESTION_TEXT,QUESTION_ANSWER,QUESTION_LIKES,QUESTION_CREATE_DATE,QUESTION_CREATE_USER,QUESTION_UPDATE_DATE,QUESTION_UPDATE_USER)
-VALUES (
-1,
-'Identify the correct methods in the Object Class.',
-'clone() , equals(Object obj) , finalize() , getClass(), hashCode() , notify() , notifyAll() , toString() ,wait(),wait(long timeout),wait(long timeout, int nanos)  are methods in java.lang.Object class.	
-
-All Java Classes implicitly have access to these methods and is the good practice to atleast overide, equals, hashCode and toString methods. 
-',
-0,
-CURRENT_TIMESTAMP,
-1,
-CURRENT_TIMESTAMP,
-1
-);		
--- Question 3 Answer Options
-INSERT INTO ANSWER_OPTION (QUESTION_ID, OPTION_TEXT, ISCORRECT) 
-VALUES (3, 
-'reverseOrder() , emptyList() , emptyMap() , emptySet(),  getClass().', 
-0
-);
-INSERT INTO ANSWER_OPTION (QUESTION_ID, OPTION_TEXT, ISCORRECT) 
-VALUES (3, 
-'clone() , equals(Object obj) , hashCode(), getClass() , toString()', 
-1
-);
-INSERT INTO ANSWER_OPTION (QUESTION_ID, OPTION_TEXT, ISCORRECT) 
-VALUES (3, 
-'clear(),isEmpty(),size(),toArray()', 
-0
-);
-INSERT INTO ANSWER_OPTION (QUESTION_ID, OPTION_TEXT, ISCORRECT) 
-VALUES (3, 
-'abs(double a) , ceil(double a), floor(double a)', 
-0
-);
--- Question 3 Answer Reference Media
-INSERT INTO ANSWER_REFERENCE_MEDIA (QUESTION_ID, REF_MEDIA_TYPE, REF_MEDIA_LINK_URL) 
-VALUES (3,
-'url',
-'https://docs.oracle.com/javase/6/docs/api/java/lang/Object.html'
-);
-
--- Question 4
-INSERT INTO QUESTION (CAT_ID, QUESTION_TEXT,QUESTION_ANSWER,QUESTION_LIKES,QUESTION_CREATE_DATE,QUESTION_CREATE_USER,QUESTION_UPDATE_DATE,QUESTION_UPDATE_USER)
-VALUES (
-1,
-'Identify which is not part of the Java Collections API Core Interfaces. ',
-'Java Collections API is made up of 7 core interfaces (Collection, Set, SortedSet, List, Queue, Map, SortedMap).	
-',
-0,
-CURRENT_TIMESTAMP,
-1,
-CURRENT_TIMESTAMP,
-1
-);		
--- Question 4 Answer Options
-INSERT INTO ANSWER_OPTION (QUESTION_ID, OPTION_TEXT, ISCORRECT) 
-VALUES (4, 
-'ArrayList,List, HashSet,Set,HashMap,Map,ArrayBlockingQueue,Queue.', 
-1
-);
-INSERT INTO ANSWER_OPTION (QUESTION_ID, OPTION_TEXT, ISCORRECT) 
-VALUES (4, 
-'List, Set,Map,Queue,SortedSet, SortedMap', 
-0
-);
-INSERT INTO ANSWER_OPTION (QUESTION_ID, OPTION_TEXT, ISCORRECT) 
-VALUES (4, 
-'Set,SortedSet, SortedMap', 
-0
-);
-INSERT INTO ANSWER_OPTION (QUESTION_ID, OPTION_TEXT, ISCORRECT) 
-VALUES (4, 
-'Map,Queue', 
-0
-);
--- Question 4 Answer Reference Media
-INSERT INTO ANSWER_REFERENCE_MEDIA (QUESTION_ID, REF_MEDIA_TYPE, REF_MEDIA_LINK_URL) 
-VALUES (4,
-'url',
-'https://docs.oracle.com/javase/6/docs/api/java/util/Collection.html'
-);
-INSERT INTO ANSWER_REFERENCE_MEDIA (QUESTION_ID, REF_MEDIA_TYPE, REF_MEDIA_LINK_URL) 
-VALUES (4,
-'url',
-'https://docs.oracle.com/javase/tutorial/collections/interfaces/index.html'
-);
-
--- Question 5
-INSERT INTO QUESTION (CAT_ID, QUESTION_TEXT,QUESTION_ANSWER,QUESTION_LIKES,QUESTION_CREATE_DATE,QUESTION_CREATE_USER,QUESTION_UPDATE_DATE,QUESTION_UPDATE_USER)
-VALUES (
-1,
-'Identify difference exist between Iterator and ListIterator.',
-'An Iterator can be used to traverse the Set and List collections, while the ListIterator can be used to iterate only over Lists.
-
-The Iterator can traverse a collection only in forward direction, while the ListIterator can traverse a List in both directions.
-
-The ListIterator implements the Iterator interface and contains extra functionality, such as adding an element, replacing an element, getting the index position for previous and next elements.
-',
-0,
-CURRENT_TIMESTAMP,
-1,
-CURRENT_TIMESTAMP,
-1
-);		
--- Question 5 Answer Options
-INSERT INTO ANSWER_OPTION (QUESTION_ID, OPTION_TEXT, ISCORRECT) 
-VALUES (5, 
-'An Iterator can be used to traverse the Set and List collections, while the ListIterator can be used to iterate only over Lists.', 
-1
-);
-INSERT INTO ANSWER_OPTION (QUESTION_ID, OPTION_TEXT, ISCORRECT) 
-VALUES (5, 
-'Both Iterator and  ListIterator can be used to traverse over collections bith have similar capabilties, ListIterator was introduced in Java 5 and supports annotations.', 
-0
-);
--- Question 5 Answer Reference Media
-INSERT INTO ANSWER_REFERENCE_MEDIA (QUESTION_ID, REF_MEDIA_TYPE, REF_MEDIA_LINK_URL) 
-VALUES (5,
-'url',
-'https://docs.oracle.com/javase/6/docs/api/java/util/Iterator.html'
-);
-INSERT INTO ANSWER_REFERENCE_MEDIA (QUESTION_ID, REF_MEDIA_TYPE, REF_MEDIA_LINK_URL) 
-VALUES (5,
-'url',
-' https://docs.oracle.com/javase/6/docs/api/java/util/ListIterator.html'
-);
-
-
--- Add Quiz for Default Java Quiz
-
--- Add Quiz
-INSERT INTO QUIZ (QUIZ_NAME, QUIZ_DESC, CAT_ID, QUIZ_LIKES, MAX_ATTEMPTS, QUIZ_CREATE_DATE,QUIZ_CREATE_USER,QUIZ_UPDATE_DATE, QUIZ_UPDATE_USER)
-VALUES ('Java Quiz', 'Default Java Quiz', 1, 0, 0, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP, 1);
-
-INSERT INTO QUIZ (QUIZ_NAME, QUIZ_DESC, CAT_ID, QUIZ_LIKES, MAX_ATTEMPTS, QUIZ_CREATE_DATE,QUIZ_CREATE_USER,QUIZ_UPDATE_DATE, QUIZ_UPDATE_USER)
-VALUES ('Dummy Quiz 1', 'Dummy  Quiz 1', 1, 0, 0, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP, 1);
-INSERT INTO QUIZ (QUIZ_NAME, QUIZ_DESC, CAT_ID, QUIZ_LIKES, MAX_ATTEMPTS, QUIZ_CREATE_DATE,QUIZ_CREATE_USER,QUIZ_UPDATE_DATE, QUIZ_UPDATE_USER)
-VALUES ('Dummy Quiz 2', 'Dummy  Quiz 2', 1, 0, 0, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP, 1);
-INSERT INTO QUIZ (QUIZ_NAME, QUIZ_DESC, CAT_ID, QUIZ_LIKES, MAX_ATTEMPTS, QUIZ_CREATE_DATE,QUIZ_CREATE_USER,QUIZ_UPDATE_DATE, QUIZ_UPDATE_USER)
-VALUES ('Dummy Quiz 3', 'Dummy  Quiz 3', 1, 0, 0, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP, 1);
-INSERT INTO QUIZ (QUIZ_NAME, QUIZ_DESC, CAT_ID, QUIZ_LIKES, MAX_ATTEMPTS, QUIZ_CREATE_DATE,QUIZ_CREATE_USER,QUIZ_UPDATE_DATE, QUIZ_UPDATE_USER)
-VALUES ('Dummy Quiz 4', 'Dummy  Quiz 4', 1, 0, 0, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP, 1);
-INSERT INTO QUIZ (QUIZ_NAME, QUIZ_DESC, CAT_ID, QUIZ_LIKES, MAX_ATTEMPTS, QUIZ_CREATE_DATE,QUIZ_CREATE_USER,QUIZ_UPDATE_DATE, QUIZ_UPDATE_USER)
-VALUES ('Dummy Quiz 5', 'Dummy  Quiz 5', 1, 0, 0, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP, 1);
-
-
-
-
--- Add Quiz Questions
-INSERT INTO QUIZ_QUESTION (QUIZ_ID, QUESTION_ID) 
-VALUES (1, 1);
-INSERT INTO QUIZ_QUESTION (QUIZ_ID, QUESTION_ID) 
-VALUES (1, 2);
-INSERT INTO QUIZ_QUESTION (QUIZ_ID, QUESTION_ID) 
-VALUES (1, 3);
-INSERT INTO QUIZ_QUESTION (QUIZ_ID, QUESTION_ID) 
-VALUES (1, 4);
-INSERT INTO QUIZ_QUESTION (QUIZ_ID, QUESTION_ID) 
-VALUES (1, 5);
 
