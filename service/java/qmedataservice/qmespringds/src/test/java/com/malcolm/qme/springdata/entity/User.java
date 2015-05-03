f96aac1dbb1a636da0a7e1f85a627eb6811da8d2
Malcolm Pereira <malcolm.j.pereira@gmail.com>
2015-05-03 12:44:15 -0400

    Updates to add spring data JPA dependencies

---
+++ service/java/qmedataservice/qmespringds/src/test/java/com/malcolm/qme/springdata/entity/User.java
@@ -0,0 +1,18 @@
+/**
+ * Name      : com.malcolm.qme.springdata.entity.User.java
+ * Date      : 5/3/15
+ * Developer : Malcolm
+ * Purpose   : QMe User Entity Object
+ */
+package com.malcolm.qme.springdata.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @Author: Malcolm
+ */
+@Entity
+@Table(name="USER")
+public class User {
+}
