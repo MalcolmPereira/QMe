acc093d60d997e96ec90edcf390aa2e40f826a9c
Malcolm Pereira <malcolm.j.pereira@gmail.com>
2015-05-17 21:30:58 -0400

    Updates for Test Cases

--- service/java/qmedataservice/qmespringds/src/main/java/com/malcolm/qme/springdata/repository/UserQuizLikesRepositoryImpl.java
+++ service/java/qmedataservice/qmespringds/src/main/java/com/malcolm/qme/springdata/repository/UserQuizLikesRepositoryImpl.java
@@ -7,16 +7,16 @@
 
 package com.malcolm.qme.springdata.repository;
 
-import com.malcolm.qme.core.domain.UserQuizLikes;
-import com.malcolm.qme.core.repository.UserQuizLikesRepository;
-import com.malcolm.qme.springdata.entity.UserQuizLikesEntity;
-import com.malcolm.qme.springdata.entity.UserQuizLikesEntityId;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.malcolm.qme.core.domain.UserQuizLikes;
+import com.malcolm.qme.core.repository.UserQuizLikesRepository;
+import com.malcolm.qme.springdata.entity.UserQuizLikesEntity;
+import com.malcolm.qme.springdata.entity.UserQuizLikesEntityId;
 
 /**
  * @Author: Malcolm
@@ -24,113 +24,113 @@ import java.util.List;
 @Repository("UserQuizLikesRepository")
 public class UserQuizLikesRepositoryImpl implements UserQuizLikesRepository {
 
-    /**
-     * Spring Data UserEntity Repository
-     */
-    @Autowired
-    private UserQuizLikesSpringDataRepository userQuizLikesSpringDataRepository;
+	/**
+	 * Spring Data UserEntity Repository
+	 */
+	@Autowired
+	private UserQuizLikesSpringDataRepository userQuizLikesSpringDataRepository;
 
-    @Override
-    public List<UserQuizLikes> findByUserId(Long userID) {
-        return null;
-    }
+	@Override
+	public List<UserQuizLikes> findByUserId(Long userID) {
+		return (getUserQuizLikes(userQuizLikesSpringDataRepository.findByUserId(userID)));
+	}
 
-    @Override
-    public List<UserQuizLikes> findByQuizId(Long quizID) {
-        return null;
-    }
+	@Override
+	public List<UserQuizLikes> findByQuizId(Long quizID) {
+		return (getUserQuizLikes(userQuizLikesSpringDataRepository.findByQuizId(quizID)));
+	}
 
-    @Override
-    public List<UserQuizLikes> findAll() {
-    	return (getUserQuizLikes(userQuizLikesSpringDataRepository.findAll()));
-    }
+	@Override
+	public List<UserQuizLikes> findAll() {
+		return (getUserQuizLikes(userQuizLikesSpringDataRepository.findAll()));
+	}
 
-    @Override
-    public UserQuizLikes findById(UserQuizLikes userQuizLikes) {
-    	UserQuizLikesEntityId userQuizLikesEntityId = getUserQuizLikesEntityId(userQuizLikes);
-    	UserQuizLikesEntity userQuizLikesEntity = userQuizLikesSpringDataRepository.findOne(userQuizLikesEntityId);
-    	if(userQuizLikesEntity != null){
-    		return getUserQuizLikes(userQuizLikesEntity);
-    	}
-        return null;
-    }
-
-    @Override
-    public UserQuizLikes save(UserQuizLikes userQuizLikes) {
-    	UserQuizLikesEntity userQuizLikesEntity = getUserQuizLikesEntity(userQuizLikes);
-    	userQuizLikesEntity = userQuizLikesSpringDataRepository.save(userQuizLikesEntity);
-    	return getUserQuizLikes(userQuizLikesEntity);
-    }
-
-    @Override
-    public UserQuizLikes update(UserQuizLikes userQuizLikes, Long updateUserId) {
-    	UserQuizLikesEntity userQuizLikesEntity = getUserQuizLikesEntity(userQuizLikes);
-    	userQuizLikesEntity = userQuizLikesSpringDataRepository.save(userQuizLikesEntity);
-    	return getUserQuizLikes(userQuizLikesEntity);
-    }
-
-    @Override
-    public void delete(UserQuizLikes userQuizLikes) {
-    	UserQuizLikesEntityId userQuizLikesEntityId = getUserQuizLikesEntityId(userQuizLikes);
-    	userQuizLikesSpringDataRepository.delete(userQuizLikesEntityId);
-    }
-    
-    
-    /**
-     * Map UserQuizLikes Domain Object to UserQuizLikesEntityId 
-     * 
-     * @param userQuizLikes
-     * @return
-     */
-    private UserQuizLikesEntityId getUserQuizLikesEntityId(UserQuizLikes userQuizLikes) {
-    	UserQuizLikesEntityId userQuizLikesEntityId = new UserQuizLikesEntityId();
-    	userQuizLikesEntityId.setUserId(userQuizLikes.getUserID());
-    	userQuizLikesEntityId.setQuizId(userQuizLikes.getQuizID());
-    	return userQuizLikesEntityId;
-    }
-    
-    /**
-     * Map UserQuizLikes Domain Object to UserQuizLikesEntity 
-     * 
-     * @param userQuizLikes
-     * @return
-     */
-    private UserQuizLikesEntity getUserQuizLikesEntity(UserQuizLikes userQuizLikes) {
-    	UserQuizLikesEntity userQuizLikesEntity = new UserQuizLikesEntity();
-    	UserQuizLikesEntityId userQuizLikesEntityId = new UserQuizLikesEntityId();
-    	userQuizLikesEntityId.setUserId(userQuizLikes.getUserID());
-    	userQuizLikesEntityId.setQuizId(userQuizLikes.getQuizID());
-    	userQuizLikesEntity.setId(userQuizLikesEntityId);
-    	return userQuizLikesEntity;
-    }
-    
-    /**
-     * Map UserQuizLikesEntity to UserQuizLikes Domain Object
-     * 
-     * @param userQuizLikesEntities
-     * @return
-     */
-    private List<UserQuizLikes> getUserQuizLikes(List<UserQuizLikesEntity> userQuizLikesEntities) {
-    	List<UserQuizLikes> userQuizLikesList = new ArrayList<UserQuizLikes>();
-    	if (userQuizLikesEntities == null) {
-			return userQuizLikesList;
+	@Override
+	public UserQuizLikes findById(UserQuizLikes userQuizLikes) {
+		final UserQuizLikesEntityId userQuizLikesEntityId = getUserQuizLikesEntityId(userQuizLikes);
+		final UserQuizLikesEntity userQuizLikesEntity = userQuizLikesSpringDataRepository.findOne(userQuizLikesEntityId);
+		if(userQuizLikesEntity != null){
+			return getUserQuizLikes(userQuizLikesEntity);
 		}
-    	for (UserQuizLikesEntity userQuizLikesEntity : userQuizLikesEntities) {
-    		userQuizLikesList.add(getUserQuizLikes(userQuizLikesEntity));
-		}
-    	return userQuizLikesList;
-    }
-    
-    /**
-     * Map UserQuizLikesEntity to UserQuizLikes Domain Object
-     * 
-     * @param userQuizLikesEntity
-     * @return
-     */
-    private UserQuizLikes getUserQuizLikes(UserQuizLikesEntity userQuizLikesEntity) {
-    	return new UserQuizLikes(
-    			userQuizLikesEntity.getId().getUserId(),
-    			userQuizLikesEntity.getId().getQuizId()
-    	);
-    }
+		return null;
+	}
+
+	@Override
+	public UserQuizLikes save(UserQuizLikes userQuizLikes) {
+		UserQuizLikesEntity userQuizLikesEntity = getUserQuizLikesEntity(userQuizLikes);
+		userQuizLikesEntity = userQuizLikesSpringDataRepository.save(userQuizLikesEntity);
+		return getUserQuizLikes(userQuizLikesEntity);
+	}
+
+	@Override
+	public UserQuizLikes update(UserQuizLikes userQuizLikes, Long updateUserId) {
+		UserQuizLikesEntity userQuizLikesEntity = getUserQuizLikesEntity(userQuizLikes);
+		userQuizLikesEntity = userQuizLikesSpringDataRepository.save(userQuizLikesEntity);
+		return getUserQuizLikes(userQuizLikesEntity);
+	}
+
+	@Override
+	public void delete(UserQuizLikes userQuizLikes) {
+		final UserQuizLikesEntityId userQuizLikesEntityId = getUserQuizLikesEntityId(userQuizLikes);
+		userQuizLikesSpringDataRepository.delete(userQuizLikesEntityId);
+	}
+
+
+	/**
+	 * Map UserQuizLikes Domain Object to UserQuizLikesEntityId
+	 *
+	 * @param userQuizLikes
+	 * @return
+	 */
+	 private UserQuizLikesEntityId getUserQuizLikesEntityId(UserQuizLikes userQuizLikes) {
+		 final UserQuizLikesEntityId userQuizLikesEntityId = new UserQuizLikesEntityId();
+		 userQuizLikesEntityId.setUserId(userQuizLikes.getUserID());
+		 userQuizLikesEntityId.setQuizId(userQuizLikes.getQuizID());
+		 return userQuizLikesEntityId;
+	 }
+
+	 /**
+	  * Map UserQuizLikes Domain Object to UserQuizLikesEntity
+	  *
+	  * @param userQuizLikes
+	  * @return
+	  */
+	 private UserQuizLikesEntity getUserQuizLikesEntity(UserQuizLikes userQuizLikes) {
+		 final UserQuizLikesEntity userQuizLikesEntity = new UserQuizLikesEntity();
+		 final UserQuizLikesEntityId userQuizLikesEntityId = new UserQuizLikesEntityId();
+		 userQuizLikesEntityId.setUserId(userQuizLikes.getUserID());
+		 userQuizLikesEntityId.setQuizId(userQuizLikes.getQuizID());
+		 userQuizLikesEntity.setId(userQuizLikesEntityId);
+		 return userQuizLikesEntity;
+	 }
+
+	 /**
+	  * Map UserQuizLikesEntity to UserQuizLikes Domain Object
+	  *
+	  * @param userQuizLikesEntities
+	  * @return
+	  */
+	 private List<UserQuizLikes> getUserQuizLikes(List<UserQuizLikesEntity> userQuizLikesEntities) {
+		 final List<UserQuizLikes> userQuizLikesList = new ArrayList<UserQuizLikes>();
+		 if (userQuizLikesEntities == null) {
+			 return userQuizLikesList;
+		 }
+		 for (final UserQuizLikesEntity userQuizLikesEntity : userQuizLikesEntities) {
+			 userQuizLikesList.add(getUserQuizLikes(userQuizLikesEntity));
+		 }
+		 return userQuizLikesList;
+	 }
+
+	 /**
+	  * Map UserQuizLikesEntity to UserQuizLikes Domain Object
+	  *
+	  * @param userQuizLikesEntity
+	  * @return
+	  */
+	 private UserQuizLikes getUserQuizLikes(UserQuizLikesEntity userQuizLikesEntity) {
+		 return new UserQuizLikes(
+				 userQuizLikesEntity.getId().getUserId(),
+				 userQuizLikesEntity.getId().getQuizId()
+				 );
+	 }
 }
--- service/java/qmedataservice/qmespringds/src/test/java/com/malcolm/qme/springdata/repository/UserQuizLikesRepositoryImplTest.java
+++ service/java/qmedataservice/qmespringds/src/test/java/com/malcolm/qme/springdata/repository/UserQuizLikesRepositoryImplTest.java
@@ -6,6 +6,14 @@
  */
 package com.malcolm.qme.springdata.repository;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -15,6 +23,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
+import com.malcolm.qme.core.domain.UserQuizLikes;
+import com.malcolm.qme.core.repository.CategoryRepository;
+import com.malcolm.qme.core.repository.QuizRepository;
+import com.malcolm.qme.core.repository.UserQuizLikesRepository;
 import com.malcolm.qme.core.repository.UserRepository;
 import com.malcolm.qme.springdata.config.QMeSpringDataJPAConfig;
 
@@ -28,7 +40,6 @@ import com.malcolm.qme.springdata.config.QMeSpringDataJPAConfig;
 		TransactionalTestExecutionListener.class
 })
 public class UserQuizLikesRepositoryImplTest {
-
 	/**
 	 * Spring Data UserEntity Repository
 	 */
@@ -36,5 +47,43 @@ public class UserQuizLikesRepositoryImplTest {
 	@Qualifier("UserRepository")
 	private UserRepository userRepo;
 
+	/**
+	 * Quiz Repository
+	 */
+	@Autowired
+	@Qualifier("QuizRepository")
+	private QuizRepository quizRepository;
+
+	/**
+	 * Category Repository
+	 */
+	@Autowired
+	@Qualifier("CategoryRepository")
+	private CategoryRepository categoryRepo;
+
+	/**
+	 * UserQuizLikes Repository
+	 */
+	@Autowired
+	@Qualifier("UserQuizLikesRepository")
+	private UserQuizLikesRepository userQuizLikesRepository;
+
+	@Test
+	public void testFindAll(){
+		assertNotNull(userQuizLikesRepository);
+		final List<UserQuizLikes> userQuizLikesList = userQuizLikesRepository.findAll();
+		assertNotNull(userQuizLikesList);
+		assertThat(userQuizLikesList.size(), greaterThan(0));
+	}
+
+	@Test
+	public void testFindById(){
+		assertNotNull(userQuizLikesRepository);
+		UserQuizLikes userQuizLikes = new UserQuizLikes(1L,1L);
+		userQuizLikes = userQuizLikesRepository.findById(userQuizLikes);
+		assertNotNull(userQuizLikes);
+		assertThat(userQuizLikes.getUserID(), equalTo(1L));
+		assertThat(userQuizLikes.getQuizID(), equalTo(1L));
+	}
 
 }
