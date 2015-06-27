b416c9cc5718932dc524740c6a3f0cf9f5f943c4
Malcolm Pereira <malcolm.j.pereira@gmail.com>
2015-06-27 17:02:51 -0400

    updates for reister user

--- client/angular-dart/qmeAngularDart/lib/component/qme_register.dart
+++ client/angular-dart/qmeAngularDart/lib/component/qme_register.dart
@@ -4,4 +4,22 @@ part of qme;
     selector: 'qme-register',
     templateUrl: 'packages/qme/component/qme_register.html',
     useShadowDom: false)
-class QMeRegisterComponent {}
+class QMeRegisterComponent {
+
+    QMeUser user = new QMeUser();
+
+    NgForm registerForm;
+
+    //Router router;
+
+    //QMeRegisterComponent(Router router) {
+    //    this.router = router;
+    //}
+
+    void registerUser() {
+        print(user.userEmail);
+        print(user.userPassword);
+        QmeErrorHolder.instance.setError("some error message");
+    }
+
+}
--- client/angular-dart/qmeAngularDart/lib/model/qme_user.dart
+++ client/angular-dart/qmeAngularDart/lib/model/qme_user.dart
@@ -2,5 +2,9 @@ part of qme;
 
 class QMeUser {
   String userEmail;
+  String userName;
   String userPassword;
+  String userPasswordConfirm;
+  String userFirstName;
+  String userLastName;
 }
