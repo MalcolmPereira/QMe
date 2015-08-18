6be4c0bc3106ff9b2115894d0a032b9d52c974a2
malcolm.j.pereira <malcolm.j.pereira@gmail.com>
2015-08-18 17:09:26 -0400

    updates to basic routing

--- client/angular/qmeAngular/app/js/app.js
+++ client/angular/qmeAngular/app/js/app.js
@@ -3,6 +3,23 @@ var qmeApp;
 (function () {
     'use strict';
 
-    qmeApp = angular.module('qmeApp', []);
+    qmeApp = angular.module('qmeApp', ['ngRoute']).config(config);
+
+    config.$inject = ['$routeProvider', '$locationProvider'];
+
+    function config($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/register', {
+                controller: 'qmeRegister',
+                templateUrl: 'js/components/register/qmeregister.html',
+                controllerAs: 'qmeRegister'
+            })
+            .when('/home', {
+                controller: 'qmeHome',
+                templateUrl: 'js/components/home/qmemain.html',
+                controllerAs: 'qmeHome'
+            })
+            .otherwise({ redirectTo: '/home' });
+    }
 
 })();
---
+++ client/angular/qmeAngular/app/js/components/home/qmemain.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+
+    qmeApp.controller('qmeHome', QMeHomeController);
+
+    QMeHomeController.$inject = ['qmeFlashService'];
+
+    function QMeHomeController(qmeFlashService) {
+
+        var qmeHome = this;
+
+    }
+
+})();
