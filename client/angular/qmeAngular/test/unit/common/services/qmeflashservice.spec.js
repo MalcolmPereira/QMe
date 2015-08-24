8086e05f50b667d912e0576692978a259ac89e47
malcolm.j.pereira <malcolm.j.pereira@gmail.com>
2015-08-24 15:29:08 -0400

    updates to add test cases

--- client/angular/qmeAngular/test/unit/common/services/qmeflashservice.spec.js
+++ client/angular/qmeAngular/test/unit/common/services/qmeflashservice.spec.js
@@ -3,15 +3,71 @@
     'use strict';
 
     describe('Service: QMe Flash Service', function() {
-
-        var qmeAuthService;
+        var scope,qmeFlashService;
 
         beforeEach(module('qmeApp'));
 
+        beforeEach(inject(function($rootScope, _qmeFlashService_) {
+            scope = $rootScope.$new();
+            qmeFlashService = _qmeFlashService_;
+        }));
 
+        it('Should have a QMe Flash Service', function() {
+            expect(qmeFlashService).toBeDefined();
+            expect(scope.flash).not.toBeDefined();
+        });
 
+        it('Should have a No Messages', function() {
+            expect(qmeFlashService).toBeDefined();
+            expect(scope.flash).not.toBeDefined();
+            qmeFlashService.Clear();
+            expect(scope.flash).not.toBeDefined();
+
+        });
+
+        it('Should have a Success Messages', function() {
+            expect(qmeFlashService).toBeDefined();
+            expect(scope.flash).not.toBeDefined();
+            qmeFlashService.Clear();
+            expect(scope.flash).not.toBeDefined();
+            qmeFlashService.Success("Some Test Message");
+            expect(scope.flash).toBeDefined();
+            expect(scope.flash.type).toBeDefined();
+            expect(scope.flash.type).toBe('success');
+            expect(scope.flash.message).toBeDefined();
+            expect(scope.flash.message).toBe('Some Test Message');
+            expect(scope.flash.keepAfterLocationChange).not.toBeDefined();
+            qmeFlashService.Success("Some Test Message",false);
+            expect(scope.flash).toBeDefined();
+            expect(scope.flash.type).toBeDefined();
+            expect(scope.flash.type).toBe('success');
+            expect(scope.flash.message).toBeDefined();
+            expect(scope.flash.message).toBe('Some Test Message');
+            expect(scope.flash.keepAfterLocationChange).toBeDefined();
+            expect(scope.flash.keepAfterLocationChange).toBe(false);
+        });
+
+        it('Should have a Error Messages', function() {
+            expect(qmeFlashService).toBeDefined();
+            expect(scope.flash).not.toBeDefined();
+            qmeFlashService.Clear();
+            expect(scope.flash).not.toBeDefined();
+            qmeFlashService.Error("Some Error Message");
+            expect(scope.flash).toBeDefined();
+            expect(scope.flash.type).toBeDefined();
+            expect(scope.flash.type).toBe('error');
+            expect(scope.flash.message).toBeDefined();
+            expect(scope.flash.message).toBe('Some Error Message');
+            expect(scope.flash.keepAfterLocationChange).not.toBeDefined();
+            qmeFlashService.Error("Some Error Message",false);
+            expect(scope.flash).toBeDefined();
+            expect(scope.flash.type).toBeDefined();
+            expect(scope.flash.type).toBe('error');
+            expect(scope.flash.message).toBeDefined();
+            expect(scope.flash.message).toBe('Some Error Message');
+            expect(scope.flash.keepAfterLocationChange).toBeDefined();
+            expect(scope.flash.keepAfterLocationChange).toBe(false);
+        });
     });
-
-
 })();
 
