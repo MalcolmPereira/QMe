9492163a6f1bd48742386a796c499d786284f1a4
malcolm.j.pereira <malcolm.j.pereira@gmail.com>
2015-09-23 15:15:30 -0400

    Updates for test cases for security module

---
+++ service/java/qmedataservice/qmecore/src/test/java/com/malcolm/qme/core/repository/QMeExceptionTest.java
@@ -0,0 +1,42 @@
+/**
+ * Name      : com.malcolm.qme.core.domain.QMeExceptionTest.java
+ * Date      : 9/23/15
+ * Developer : Malcolm
+ * Purpose   : QMe Exception Class Test
+ */
+package com.malcolm.qme.core.repository;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertNotNull;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Malcolm
+ */
+public class QMeExceptionTest{
+
+    @Test
+    public void testGetErrorMessage(){
+        QMeException qmeErr = new QMeException("Some Error Message");
+        assertThat(qmeErr.getErrorMessage(),equalTo("Some Error Message"));
+        qmeErr = new QMeException(new Exception("Some Error Message"));
+        assertThat(qmeErr.getErrorMessage(),equalTo("Some Error Message"));
+        qmeErr = new QMeException("Some Error Message", new Exception("Some Error Message"));
+        assertThat(qmeErr.getErrorMessage(),equalTo("Some Error Message"));
+    }
+
+    @Test
+    public void testGetError() {
+        QMeException qmeErr = new QMeException("Some Error Message");
+        assertNull(qmeErr.getError());
+        qmeErr = new QMeException(new Exception("Some Error Message"));
+        assertNotNull(qmeErr.getError());
+        assertThat(qmeErr.getErrorMessage(), equalTo("Some Error Message"));
+        qmeErr = new QMeException("Some Error Message", new Exception("Some Error Message"));
+        assertNotNull(qmeErr.getError());
+        assertThat(qmeErr.getErrorMessage(), equalTo("Some Error Message"));
+    }
+}
