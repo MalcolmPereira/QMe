1116438b2243c542a60b6db4acceb5d84b6bb68a
malcolm.j.pereira <malcolm.j.pereira@gmail.com>
2015-09-30 09:43:51 -0400

    updates to include new user controller for all user feature

---
+++ client/angular/qmeAngular/app/js/user/qmeuserctrl.js
@@ -0,0 +1,196 @@
+(function () {
+
+    'use strict';
+
+    ngQMe
+        .controller('qmeUserCtrl', QMeUserController);
+
+    QMeUserController.$inject = ['$state','$stateParams','qmeFlashService','qmeAuthService','qmeUserService'];
+
+    function QMeUserController($state,$stateParams,qmeFlashService,qmeAuthService,qmeUserService) {
+
+        var qmeUser = this;
+
+        qmeUser.userEmail = "";
+        qmeUser.userName = "";
+        qmeUser.userPassword = "";
+        qmeUser.userPasswordConfirm = "";
+        qmeUser.userFirstName = "";
+        qmeUser.userLastName = "";
+
+        qmeUser.isSignedIn = function(){
+            return qmeAuthService.isSignedIn();
+        };
+
+        qmeUser.isAdmin = function(){
+            return qmeAuthService.isAdmin();
+        };
+
+        qmeUser.userName = function (){
+            if(qmeUser.isSignedIn()){
+                if(qmeAuthService.user() && qmeAuthService.user().userfirstname() && qmeAuthService.user().userfirstname().length > 0
+                    && qmeAuthService.user().userlastname() && qmeAuthService.user().userlastname().length > 0
+                ){
+                    return   qmeAuthService.user().userfirstname() + " "+ qmeAuthService.user().userlastname();
+
+                }else if(qmeAuthService.user() && qmeAuthService.user().userfirstname() && qmeAuthService.user().userfirstname().length > 0){
+                    return   qmeAuthService.user().userfirstname();
+
+                }else{
+                    return qmeAuthService.username();
+                }
+            }
+            return "";
+        };
+
+
+        qmeUser.performSignIn = function (){
+
+            var credentials;
+            qmeFlashService.Clear();
+
+            credentials = {
+                "username": qmeUser.userEmail,
+                "password": qmeUser.userPassword
+            };
+
+            qmeAuthService.login(credentials)
+
+                .then(
+
+                function(res){
+                    $state.go('home', {});
+                },
+                function(error){
+                    if(error && error.status && error.status == 401){
+                        qmeFlashService.Error("Oops.....User not authorized, please register or click on forgot password.");
+
+                    }else if(error && error.status && error.status == 404){
+                        qmeFlashService.Error("Error Connecting to service, entered user credential not found.");
+
+                    }else{
+                        qmeFlashService.Error("Oops.....Error Connecting to service, please retry in some time.");
+                    }
+                }
+            );
+        };
+
+        qmeUser.logout = function (){
+            qmeAuthService.logout();
+            qmeAuthService.endRegistering();
+            qmeAuthService.endResetting();
+            qmeUser.userEmail = "";
+            qmeUser.userPassword = "";
+            qmeUser.signInForm.$setPristine()
+        };
+
+        qmeUser.routeRegistration = function (){
+            qmeAuthService.startRegistering();
+            $state.go('register', {});
+        };
+
+        qmeUser.routeResetPassword = function (){
+            qmeAuthService.startResetting();
+            $state.go('reset', {});
+        };
+
+        qmeUser.cancelResetRegistration = function (){
+            qmeAuthService.endRegistering();
+            qmeAuthService.endResetting();
+            qmeUser.signInForm.$setPristine()
+            $state.go('home', {});
+        };
+
+        qmeUser.isRegistering = function(){
+            return  qmeAuthService.isRegistering();
+        }
+
+        qmeUser.isResetingPassword = function(){
+            return  qmeAuthService.isResetting();
+        }
+
+        qmeUser.validatePasswordFields = function (){
+            if(qmeUser.userPassword != qmeUser.userPasswordConfirm){
+                qmeFlashService.Error("Password do not match, please confirm password");
+                qmeUser.userPasswordConfirm ="";
+            }else{
+                qmeFlashService.Clear();
+            }
+        };
+
+        qmeUser.registerUser = function (){
+
+            qmeFlashService.Clear();
+
+            var user = {
+                "userName": qmeUser.userName,
+                "userPassword": qmeUser.userPassword ,
+                "userFirstName": qmeUser.userFirstName,
+                "userLastName": qmeUser.userLastName,
+                "userEmail": qmeUser.userEmail
+            }
+
+            qmeUserService
+                .register(user)
+                .then(
+                function(res){
+                    $state.go('home', {});
+                },
+                function(error){
+                    if(error && error.status && error.status == 400){
+                        qmeFlashService.Error("Oops.....Invalid request for user registration, please make sure all required fields are valid.");
+
+                    }else if(error && error.status && error.status == 409){
+                        qmeFlashService.Error("Oops...User with same email address already exists please enter valid unique email address.");
+
+                    }else{
+                        qmeFlashService.Error("Oops.....Error registering new user, please retry in some time.");
+                    }
+                }
+            );
+        };
+
+        qmeUser.submitReset = function(){
+
+            qmeUserService
+                .resetPassword(qmeUser.userEmail)
+                .then(
+                function (res){
+                    $state.go('home', {});
+                    qmeFlashService.Success("User password reset request submitted successfully, please validate your email address to complete reset.");
+                },
+                function (error){
+                    if(error && error.status && error.status == 404){
+                        qmeFlashService.Error("Entered user email not found. Please ener valid existing user email.");
+
+                    }else{
+                        qmeFlashService.Error("Oops.....Error connecting to service for reset password, please retry in some time.");
+                    }
+                }
+            );
+        };
+
+        qmeUser.submitResetPassword = function(){
+            qmeUserService
+                .submitResetPassword($stateParams.token,$stateParams.username,qmeUser.userEmail,qmeUser.userPassword)
+                .then(
+                function (res){
+                    $state.go('home', {});
+                },
+                function (error){
+                    if(error && error.status && error.status == 404) {
+                        qmeFlashService.Error("Entered user email not found. Please ener valid existing user email.");
+
+                    }else if(error && error.status && error.status == 400){
+                        qmeFlashService.Error("Reset token invalid, Please ener valid reset token.");
+
+                    }else{
+                        qmeFlashService.Error("Oops.....Error connecting to service for reset password, please retry in some time.");
+                    }
+                }
+            );
+            console.log("$stateParams.token",$stateParams.token);
+            console.log("$stateParams.username",$stateParams.username);
+        };
+    }
+})();
---
+++ client/angular/qmeAngular/test/unit/user/qmeuserctrl.spec.js
@@ -0,0 +1,340 @@
+(function () {
+
+    'use strict';
+
+    describe('Controller: QMe User Controller', function() {
+
+
+        var scope, state, httpBackend, ctrl, qmeContants, userAuthEndPoint,userRegisterEndpoint,logoutUserEndPoint,userForgotPaswordEndpoint;
+
+        beforeEach(module('qmeApp'));
+
+
+        beforeEach(inject(function($rootScope,$state, $controller,$httpBackend,_QME_CONSTANTS_) {
+
+            scope = $rootScope.$new();
+
+            state = $state;
+
+            httpBackend = $httpBackend;
+
+            qmeContants = _QME_CONSTANTS_;
+
+            userAuthEndPoint =  qmeContants.qmeservice+"/user/"+"searchemail/";
+
+            userRegisterEndpoint = qmeContants.qmeservice+"/user/register";
+
+            userForgotPaswordEndpoint =  qmeContants.qmeservice+"/user/"+"reset/forgotpassword/";
+
+            logoutUserEndPoint =  qmeContants.qmeservice+"/logout";
+
+            ctrl  = $controller('qmeUserCtrl', {
+                $scope: scope,
+                $state: state,
+                userEmail:"",
+                userName:"",
+                userPassword:"",
+                userPasswordConfirm:"",
+                userFirstName:"",
+                userLastName:""
+            });
+        }));
+
+        it('Should have a QMe User controller', function() {
+            expect(ctrl).toBeDefined();
+        });
+
+        it('Ensure User is not signed - signedIn is false', function() {
+            expect(ctrl.isSignedIn() ).toBe(false);
+        });
+
+        it('Ensure user is not registering - isRegistering  is false', function() {
+            expect(ctrl.isRegistering() ).toBe(false);
+        });
+
+        it('Ensure user is not resetting password  - isResetingPassword is false', function() {
+            expect(ctrl.isResetingPassword() ).toBe(false);
+        });
+
+        it('Ensure user email is empty - userEmail is empty ', function() {
+            expect(ctrl.userEmail ).toBe("");
+        });
+
+        it('Ensure user password is empty - userPassword is empty ', function() {
+            expect(ctrl.userPassword ).toBe("");
+        });
+
+        it('Ensure user able to sign-on with valid credentials for USER Role', function() {
+            ctrl.userEmail = "testuser@test.com";
+            ctrl.userPassword = "testpassword";
+            var user = {
+                "userName": "testuser",
+                "userPassword": null,
+                "userFirstName": "Test",
+                "userLastName": "User",
+                "userEmail": "test.user@gmail.com",
+                "userId": 1,
+                "userRegisteredDate": "2015-28-05 13:35:29",
+                "userUpdateDate": "2015-28-05 13:35:29",
+                "updateUserID": 0,
+                "updateUserName": "",
+                "userRoles": ['USER']
+            };
+            httpBackend.expectGET(userAuthEndPoint+ctrl.userEmail).respond(200,user);
+            httpBackend.whenGET(/js\//).respond(200,{});
+            ctrl.performSignIn();
+            httpBackend.flush();
+            expect(ctrl.userEmail ).not.toBe('');
+            expect(ctrl.userEmail ).toBe('testuser@test.com');
+            expect(ctrl.userPassword ).not.toBe('');
+            expect(ctrl.userPassword ).toBe('testpassword');
+            expect(ctrl.isSignedIn() ).toBe(true);
+            expect(ctrl.isAdmin() ).toBe(false);
+        });
+
+        it('Ensure user able to sign-on with valid credentials for ADMIN Role', function() {
+            ctrl.userEmail = "testuser@test.com";
+            ctrl.userPassword = "testpassword";
+            var user = {
+                "userName": "testadmin",
+                "userPassword": null,
+                "userFirstName": "Test",
+                "userLastName": "Admin",
+                "userEmail": "test.admin@gmail.com",
+                "userId": 1,
+                "userRegisteredDate": "2015-28-05 13:35:29",
+                "userUpdateDate": "2015-28-05 13:35:29",
+                "updateUserID": 0,
+                "updateUserName": "",
+                "userRoles": ['ADMIN','USER']
+            };
+            httpBackend.expectGET(userAuthEndPoint+ctrl.userEmail).respond(200,user);
+            httpBackend.whenGET(/js\//).respond(200,{});
+            ctrl.performSignIn();
+            httpBackend.flush();
+            expect(ctrl.userEmail ).not.toBe('');
+            expect(ctrl.userEmail ).toBe('testuser@test.com');
+            expect(ctrl.userPassword ).not.toBe('');
+            expect(ctrl.userPassword ).toBe('testpassword');
+            expect(ctrl.isSignedIn() ).toBe(true);
+            expect(ctrl.isAdmin() ).toBe(true);
+        });
+        //TODO: Write Test Cases for 404 and 401 Errors
+
+
+        it('Should have default fields available for User Registration', function() {
+            expect(ctrl.userEmail).toBeDefined();
+            expect(ctrl.userEmail).toBe("");
+            expect(ctrl.userName).toBeDefined();
+            expect(ctrl.userName()).toBe("");
+            expect(ctrl.userPassword).toBeDefined();
+            expect(ctrl.userPassword).toBe("");
+            expect(ctrl.userPasswordConfirm).toBeDefined();
+            expect(ctrl.userPasswordConfirm).toBe("");
+            expect(ctrl.userFirstName).toBeDefined();
+            expect(ctrl.userFirstName).toBe("");
+            expect(ctrl.userLastName).toBeDefined();
+            expect(ctrl.userLastName).toBe("");
+        });
+
+        it('Should check for non-matching passwords on User Registration', function() {
+            ctrl.userPassword = 'password1';
+            ctrl.userPasswordConfirm = 'password2';
+            ctrl.validatePasswordFields();
+            expect(scope.flash).toBeDefined();
+            expect(scope.flash.type).toBeDefined();
+            expect(scope.flash.type).toBe('error');
+            expect(scope.flash.message).toBeDefined();
+            expect(scope.flash.message).toBe('Password do not match, please confirm password');
+            ctrl.userPassword = 'password1';
+            ctrl.userPasswordConfirm = 'password1';
+            ctrl.validatePasswordFields();
+            ctrl.validatePasswordFields();
+            expect(scope.flash).not.toBeDefined();
+        });
+
+        it('Should handle 400 Error for invalid data on User Registration', function() {
+            ctrl.userEmail = "someemail";
+            ctrl.userName = "someusername";
+            ctrl.userPassword = "somepassword";
+            ctrl.userPasswordConfirm= "somepassword";
+            ctrl.userFirstName = "firstname";
+            ctrl.userLastName = "lastname";
+            var user = {
+                "userName": ctrl.userName,
+                "userPassword": ctrl.userPassword ,
+                "userFirstName": ctrl.userFirstName,
+                "userLastName": ctrl.userLastName,
+                "userEmail": ctrl.userEmail
+            };
+            httpBackend.expectPOST(userRegisterEndpoint,user).respond(400,{});
+            httpBackend.whenGET(/js\//).respond(200,{});
+            ctrl.registerUser();
+            httpBackend.flush();
+            expect(scope.flash).toBeDefined();
+            expect(scope.flash.type).toBeDefined();
+            expect(scope.flash.type).toBe('error');
+            expect(scope.flash.message).toBeDefined();
+            expect(scope.flash.message).toBe('Oops.....Invalid request for user registration, please make sure all required fields are valid.');
+
+        });
+
+        it('Should handle 409 Error for duplicate data on User Registration', function() {
+            ctrl.userEmail = "someemail";
+            ctrl.userName = "someusername";
+            ctrl.userPassword = "somepassword";
+            ctrl.userPasswordConfirm= "somepassword";
+            ctrl.userFirstName = "firstname";
+            ctrl.userLastName = "lastname";
+            var user = {
+                "userName": ctrl.userName,
+                "userPassword": ctrl.userPassword ,
+                "userFirstName": ctrl.userFirstName,
+                "userLastName": ctrl.userLastName,
+                "userEmail": ctrl.userEmail
+            };
+            httpBackend.expectPOST(userRegisterEndpoint,user).respond(409,{});
+            httpBackend.whenGET(/js\//).respond(200,{});
+            ctrl.registerUser();
+            httpBackend.flush();
+            expect(scope.flash).toBeDefined();
+            expect(scope.flash.type).toBeDefined();
+            expect(scope.flash.type).toBe('error');
+            expect(scope.flash.message).toBeDefined();
+            expect(scope.flash.message).toBe('Oops...User with same email address already exists please enter valid unique email address.');
+        });
+
+        it('Should handle 500 Error for server error on User Registration', function() {
+            ctrl.userEmail = "someemail";
+            ctrl.userName = "someusername";
+            ctrl.userPassword = "somepassword";
+            ctrl.userPasswordConfirm= "somepassword";
+            ctrl.userFirstName = "firstname";
+            ctrl.userLastName = "lastname";
+            var user = {
+                "userName": ctrl.userName,
+                "userPassword": ctrl.userPassword ,
+                "userFirstName": ctrl.userFirstName,
+                "userLastName": ctrl.userLastName,
+                "userEmail": ctrl.userEmail
+            };
+            httpBackend.expectPOST(userRegisterEndpoint,user).respond(500,{});
+            httpBackend.whenGET(/js\//).respond(200,{});
+            ctrl.registerUser();
+            httpBackend.flush();
+            expect(scope.flash).toBeDefined();
+            expect(scope.flash.type).toBeDefined();
+            expect(scope.flash.type).toBe('error');
+            expect(scope.flash.message).toBeDefined();
+            expect(scope.flash.message).toBe('Oops.....Error registering new user, please retry in some time.');
+        });
+
+        it('Should handle valid User Registration ', function() {
+            ctrl.userEmail = "someemail";
+            ctrl.userName = "someusername";
+            ctrl.userPassword = "somepassword";
+            ctrl.userPasswordConfirm= "somepassword";
+            ctrl.userFirstName = "firstname";
+            ctrl.userLastName = "lastname";
+            var user = {
+                "userName": ctrl.userName,
+                "userPassword": ctrl.userPassword ,
+                "userFirstName": ctrl.userFirstName,
+                "userLastName": ctrl.userLastName,
+                "userEmail": ctrl.userEmail
+            };
+            var registerdUser = {
+                "userName": "testadmin",
+                "userPassword": null,
+                "userFirstName": "Test",
+                "userLastName": "Admin",
+                "userEmail": "test.admin@gmail.com",
+                "userId": 1,
+                "userRegisteredDate": "2015-28-05 13:35:29",
+                "userUpdateDate": "2015-28-05 13:35:29",
+                "updateUserID": 0,
+                "updateUserName": "",
+                "userRoles": ['ADMIN','USER']
+            };
+            httpBackend.expectPOST(userRegisterEndpoint,user).respond(200,registerdUser);
+            httpBackend.expectPOST(logoutUserEndPoint).respond(200,user);
+            httpBackend.whenGET(/js\//).respond(200,{});
+            ctrl.registerUser();
+            httpBackend.flush();
+            expect(scope.flash).not.toBeDefined();
+            expect(state).not.toBeNull();
+            expect(state.current.name).toBe('home');
+            expect(state.current.url).toBe('/home');
+            expect(state.current.templateUrl).toBe('js/home/qmehome.tmpl.html');
+            expect(state.current.controller).toBe('qmeHomeCtrl');
+            expect(state.current.controllerAs).toBe('qmeHome');
+        });
+
+        it('Should have default fields on Forgot Password', function() {
+            expect(ctrl.userEmail).toBeDefined();
+            expect(ctrl.userEmail).toBe("");
+            expect(ctrl.userPassword).toBeDefined();
+            expect(ctrl.userPassword).toBe("");
+            expect(ctrl.userPasswordConfirm).toBeDefined();
+            expect(ctrl.userPasswordConfirm).toBe("");
+        });
+
+        it('Should check for non matching passwords on Reset Password', function() {
+            ctrl.userPassword = 'password1';
+            ctrl.userPasswordConfirm = 'password2';
+            ctrl.validatePasswordFields();
+            expect(scope.flash).toBeDefined();
+            expect(scope.flash.type).toBeDefined();
+            expect(scope.flash.type).toBe('error');
+            expect(scope.flash.message).toBeDefined();
+            expect(scope.flash.message).toBe('Password do not match, please confirm password');
+            ctrl.userPassword = 'password1';
+            ctrl.userPasswordConfirm = 'password1';
+            ctrl.validatePasswordFields();
+            ctrl.validatePasswordFields();
+            expect(scope.flash).not.toBeDefined();
+        });
+
+        it('Should handle 404 Error for user not found error on Submit Reset Password', function() {
+            ctrl.userEmail = "someemail";
+            httpBackend.expectPUT(userForgotPaswordEndpoint+ctrl.userEmail,qmeContants.reseturl).respond(404,{});
+            httpBackend.whenGET(/js\//).respond(200,{});
+            ctrl.submitReset();
+            httpBackend.flush();
+            expect(scope.flash).toBeDefined();
+            expect(scope.flash.type).toBeDefined();
+            expect(scope.flash.type).toBe('error');
+            expect(scope.flash.message).toBeDefined();
+            expect(scope.flash.message).toBe('Entered user email not found. Please ener valid existing user email.');
+        });
+
+
+        it('Should handle 500 Error for user not found error on Submit Reset Password', function() {
+            ctrl.userEmail = "someemail";
+            httpBackend.expectPUT(userForgotPaswordEndpoint+ctrl.userEmail,qmeContants.reseturl).respond(500,{});
+            httpBackend.whenGET(/js\//).respond(200,{});
+            ctrl.submitReset();
+            httpBackend.flush();
+            expect(scope.flash).toBeDefined();
+            expect(scope.flash.type).toBeDefined();
+            expect(scope.flash.type).toBe('error');
+            expect(scope.flash.message).toBeDefined();
+            expect(scope.flash.message).toBe('Oops.....Error connecting to service for reset password, please retry in some time.');
+        });
+
+        it('Should handle valid user password submit on Submit Reset Password', function() {
+            ctrl.userEmail = "someemail";
+            httpBackend.expectPUT(userForgotPaswordEndpoint+ctrl.userEmail,qmeContants.reseturl).respond(200,{});
+            httpBackend.whenGET(/js\//).respond(200,{});
+            ctrl.submitReset();
+            httpBackend.flush();
+            expect(scope.flash).toBeDefined();
+            expect(scope.flash.type).toBeDefined();
+            expect(scope.flash.type).toBe('success');
+            expect(scope.flash.message).toBeDefined();
+            expect(scope.flash.message).toBe('User password reset request submitted successfully, please validate your email address to complete reset.');
+        });
+
+    });
+
+})();
