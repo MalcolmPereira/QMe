bb56ed08d60dfe7b0c3c6af52afbddc1b1e74501
malcolm.j.pereira <malcolm.j.pereira@gmail.com>
2015-10-08 20:26:53 -0400

    updates for update user

---
+++ service/java/qmedataservice/qmerest/src/main/java/com/malcolm/qme/rest/model/QMeUpdateUser.java
@@ -0,0 +1,33 @@
+/**
+ * Name      : com.malcolm.qme.rest.model.QMeUpdateUser.java
+ * Date      : 10/08/15
+ * Developer : Malcolm
+ * Purpose   : QMe Update User (Update User Details Including User Password)
+ */
+package com.malcolm.qme.rest.model;
+
+/**
+ * @author Malcolm
+ */
+public class QMeUpdateUser extends QMeUser{
+    /**
+     * Updated User Password
+     */
+    private String updatedUserPassword;
+
+    /**
+     * Get Updated User Password
+     * @return
+     */
+    public String getUpdatedUserPassword() {
+        return updatedUserPassword;
+    }
+
+    /**
+     * Set Updated User Password
+     * @param updatedUserPassword
+     */
+    public void setUpdatedUserPassword(String updatedUserPassword) {
+        this.updatedUserPassword = updatedUserPassword;
+    }
+}
