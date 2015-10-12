41e6d80173c3af94d52029ed091df7a07d46d1fc
Malcolm Pereira <malcolm.j.pereira@gmail.com>
2015-10-12 12:54:38 -0400

    updates for z-index for multiple navbars

--- client/angular/qmeAngular/app/js/common/directives/qmeheader.js
+++ client/angular/qmeAngular/app/js/common/directives/qmeheader.js
@@ -11,6 +11,29 @@
                 controllerAs: 'qmeUserCtrl',
                 templateUrl: 'js/common/directives/qmeheader.tmpl.html'
             };
-        });
+        })
+
+        .controller('qmeNavCtrl', QMeNavigationController);
+
+        QMeNavigationController.$inject = ['$state','qmeUserSession'];
+
+        function QMeNavigationController($state,qmeUserSession) {
+
+            var qmeNav = this;
+
+            qmeNav.isSignedIn = function(){
+                return !!(qmeUserSession && qmeUserSession.isSignedIn());
+
+            };
+
+            qmeNav.isAdmin = function(){
+                return !!(qmeUserSession && qmeUserSession.isSignedIn() && qmeUserSession.isAdmin());
+
+            };
+
+            qmeNav.routeHome = function(){
+                $state.go('home', {});
+            };
+        }
 
 })();
