c2a4ce5da0b1fd3142e7c73652abca8b77bd0f33
malcolm.j.pereira <malcolm.j.pereira@gmail.com>
2015-10-28 12:21:50 -0400

    updates for test cases

--- client/angular/qmeAngular/test/unit/common/directives/qmepagination.spec.js
+++ client/angular/qmeAngular/test/unit/common/directives/qmepagination.spec.js
@@ -101,7 +101,7 @@
             expect(qmePageSession.getNextGroup()).toBe(0);
         });
 
-        //Pagination 120 Records 50 Pere Page so 3 Pages (50/50/20)
+        //Pagination 120 Records 50 Per Page so 3 Pages (50/50/20) [No Page Groupings]
         it('Should have a QMe Page Session and return valid page and page group states when total records is 120 ', function() {
             expect(qmePageSession).toBeDefined();
 
@@ -123,32 +123,318 @@
             expect(qmePageSession.getPage()).toBe(2);
             qmePageSession.getLast();
             expect(qmePageSession.getPage()).toBe(2);
-
-
-
-            /*
-            qmePageSession.setPrevious();
-            expect(qmePageSession.getFirst()).toBe(0);
-            expect(qmePageSession.getLast()).toBe(0);
-            expect(qmePageSession.getPage()).toBe(0);
-
-
-
-            qmePageSession.setPage(0);
-            expect(qmePageSession.getFirst()).toBe(0);
-            expect(qmePageSession.getLast()).toBe(0);
-            expect(qmePageSession.getPage()).toBe(0);
-
-            expect(qmePageSession.isCurrentPage(0)).toBe(true);
-            expect(qmePageSession.isFirstPage()).toBe(true);
-            expect(qmePageSession.isLastPage()).toBe(true);
             expect(qmePageSession.showPreviousGroup()).toBe(false);
-            expect(qmePageSession.getPreviousGroup()).toBe(0);
             expect(qmePageSession.showNextGroup()).toBe(false);
-            expect(qmePageSession.getNextGroup()).toBe(0);
-            */
         });
 
+        //Pagination 826 Records 50 Pere Page so 16 Pages (50/50/50/50/50/50/50/50/50/50/50/50/50/50/50/50) [Two Groupings 1 - 10 and 11 - 16]
+        it('Should have a QMe Page Session and return valid page and page group states when total records is 826 ', function() {
+            expect(qmePageSession).toBeDefined();
+
+            qmePageSession.create(826);
+            expect(qmePageSession.requiresPagination()).toBe(true);
+            expect(qmePageSession.pages()).toBeDefined();
+            expect(qmePageSession.pages().length).toBe(10);
+            expect(qmePageSession.getPage()).toBe(0);
+            expect(qmePageSession.getFirst()).toBe(0);
+            expect(qmePageSession.isCurrentPage(0)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(true);
+            expect(qmePageSession.isLastPage()).toBe(false);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+            expect(qmePageSession.getLast()).toBe(16);
+            expect(qmePageSession.getPage()).toBe(16);
+            expect(qmePageSession.isCurrentPage(16)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(true);
+            expect(qmePageSession.showPreviousGroup()).toBe(true);
+            expect(qmePageSession.showNextGroup()).toBe(false);
+
+            qmePageSession.getFirst();
+            expect(qmePageSession.getPage()).toBe(0);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+            expect(qmePageSession.isCurrentPage(0)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(true);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setNext();
+            expect(qmePageSession.getPage()).toBe(1);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+            expect(qmePageSession.isCurrentPage(1)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setNext();
+            expect(qmePageSession.getPage()).toBe(2);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+            expect(qmePageSession.isCurrentPage(2)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setNext();
+            expect(qmePageSession.getPage()).toBe(3);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+            expect(qmePageSession.isCurrentPage(3)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setNext();
+            expect(qmePageSession.getPage()).toBe(4);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+            expect(qmePageSession.isCurrentPage(4)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setNext();
+            expect(qmePageSession.getPage()).toBe(5);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+            expect(qmePageSession.isCurrentPage(5)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setNext();
+            expect(qmePageSession.getPage()).toBe(6);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+            expect(qmePageSession.isCurrentPage(6)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setNext();
+            expect(qmePageSession.getPage()).toBe(7);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+            expect(qmePageSession.isCurrentPage(7)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+
+            qmePageSession.setNext();
+            expect(qmePageSession.getPage()).toBe(8);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+            expect(qmePageSession.isCurrentPage(8)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setNext();
+            expect(qmePageSession.getPage()).toBe(9);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+            expect(qmePageSession.isCurrentPage(9)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setNext();
+            expect(qmePageSession.getPage()).toBe(10);
+            expect(qmePageSession.showPreviousGroup()).toBe(true);
+            expect(qmePageSession.showNextGroup()).toBe(false);
+            expect(qmePageSession.isCurrentPage(10)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setNext();
+            expect(qmePageSession.getPage()).toBe(11);
+            expect(qmePageSession.showPreviousGroup()).toBe(true);
+            expect(qmePageSession.showNextGroup()).toBe(false);
+            expect(qmePageSession.isCurrentPage(11)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setNext();
+            expect(qmePageSession.getPage()).toBe(12);
+            expect(qmePageSession.showPreviousGroup()).toBe(true);
+            expect(qmePageSession.showNextGroup()).toBe(false);
+            expect(qmePageSession.isCurrentPage(12)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setNext();
+            expect(qmePageSession.getPage()).toBe(13);
+            expect(qmePageSession.showPreviousGroup()).toBe(true);
+            expect(qmePageSession.showNextGroup()).toBe(false);
+            expect(qmePageSession.isCurrentPage(13)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setNext();
+            expect(qmePageSession.getPage()).toBe(14);
+            expect(qmePageSession.showPreviousGroup()).toBe(true);
+            expect(qmePageSession.showNextGroup()).toBe(false);
+            expect(qmePageSession.isCurrentPage(14)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setNext();
+            expect(qmePageSession.getPage()).toBe(15);
+            expect(qmePageSession.showPreviousGroup()).toBe(true);
+            expect(qmePageSession.showNextGroup()).toBe(false);
+            expect(qmePageSession.isCurrentPage(15)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setNext();
+            expect(qmePageSession.getPage()).toBe(16);
+            expect(qmePageSession.showPreviousGroup()).toBe(true);
+            expect(qmePageSession.showNextGroup()).toBe(false);
+            expect(qmePageSession.isCurrentPage(16)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(true);
+
+            qmePageSession.setPrevious();
+            expect(qmePageSession.getPage()).toBe(15);
+            expect(qmePageSession.showPreviousGroup()).toBe(true);
+            expect(qmePageSession.showNextGroup()).toBe(false);
+            expect(qmePageSession.isCurrentPage(15)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setPrevious();
+            expect(qmePageSession.getPage()).toBe(14);
+            expect(qmePageSession.showPreviousGroup()).toBe(true);
+            expect(qmePageSession.showNextGroup()).toBe(false);
+            expect(qmePageSession.isCurrentPage(14)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setPrevious();
+            expect(qmePageSession.getPage()).toBe(13);
+            expect(qmePageSession.showPreviousGroup()).toBe(true);
+            expect(qmePageSession.showNextGroup()).toBe(false);
+            expect(qmePageSession.isCurrentPage(13)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setPrevious();
+            expect(qmePageSession.getPage()).toBe(12);
+            expect(qmePageSession.showPreviousGroup()).toBe(true);
+            expect(qmePageSession.showNextGroup()).toBe(false);
+            expect(qmePageSession.isCurrentPage(12)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setPrevious();
+            expect(qmePageSession.getPage()).toBe(11);
+            expect(qmePageSession.showPreviousGroup()).toBe(true);
+            expect(qmePageSession.showNextGroup()).toBe(false);
+            expect(qmePageSession.isCurrentPage(11)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setPrevious();
+            expect(qmePageSession.getPage()).toBe(10);
+            expect(qmePageSession.showPreviousGroup()).toBe(true);
+            expect(qmePageSession.showNextGroup()).toBe(false);
+            expect(qmePageSession.isCurrentPage(10)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setPrevious();
+            expect(qmePageSession.getPage()).toBe(9);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+            expect(qmePageSession.isCurrentPage(9)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setPrevious();
+            expect(qmePageSession.getPage()).toBe(8);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+            expect(qmePageSession.isCurrentPage(8)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setPrevious();
+            expect(qmePageSession.getPage()).toBe(7);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+            expect(qmePageSession.isCurrentPage(7)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setPrevious();
+            expect(qmePageSession.getPage()).toBe(6);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+            expect(qmePageSession.isCurrentPage(6)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setPrevious();
+            expect(qmePageSession.getPage()).toBe(5);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+            expect(qmePageSession.isCurrentPage(5)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setPrevious();
+            expect(qmePageSession.getPage()).toBe(4);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+            expect(qmePageSession.isCurrentPage(4)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setPrevious();
+            expect(qmePageSession.getPage()).toBe(3);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+            expect(qmePageSession.isCurrentPage(3)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setPrevious();
+            expect(qmePageSession.getPage()).toBe(2);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+            expect(qmePageSession.isCurrentPage(2)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setPrevious();
+            expect(qmePageSession.getPage()).toBe(1);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+            expect(qmePageSession.isCurrentPage(1)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(false);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.setPrevious();
+            expect(qmePageSession.getPage()).toBe(0);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+            expect(qmePageSession.isCurrentPage(0)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(true);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.getFirst();
+            expect(qmePageSession.getPage()).toBe(0);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+            expect(qmePageSession.isCurrentPage(0)).toBe(true);
+            expect(qmePageSession.isFirstPage()).toBe(true);
+            expect(qmePageSession.isLastPage()).toBe(false);
+
+            qmePageSession.getNextGroup();
+            expect(qmePageSession.pages()).toBeDefined();
+            expect(qmePageSession.pages().length).toBe(7);
+            expect(qmePageSession.showPreviousGroup()).toBe(true);
+            expect(qmePageSession.showNextGroup()).toBe(false);
+
+            qmePageSession.getPreviousGroup();
+            expect(qmePageSession.pages()).toBeDefined();
+            expect(qmePageSession.pages().length).toBe(10);
+            expect(qmePageSession.showPreviousGroup()).toBe(false);
+            expect(qmePageSession.showNextGroup()).toBe(true);
+        });
     });
 
 })();
