9098e6fb6b1e78521de894d0228072951849d7fd
malcolm.j.pereira <malcolm.j.pereira@gmail.com>
2015-11-11 15:06:54 -0500

    updates for category resource

--- client/angular/qmeAngular/app/js/admin/category/qmecategorymgntctrl.js
+++ client/angular/qmeAngular/app/js/admin/category/qmecategorymgntctrl.js
@@ -6,15 +6,30 @@
 
         .controller('qmeCategoryManagementCtrl', QMeCategoryManagementController);
 
-        QMeCategoryManagementController.$inject = ['$state','qmeFlashService','qmeUserService'];
+        QMeCategoryManagementController.$inject = ['$state','qmeFlashService','qmeCategoryService'];
 
-        function QMeCategoryManagementController($state,qmeFlashService,qmeUserService) {
+        function QMeCategoryManagementController($state,qmeFlashService,qmeCategoryService) {
 
             var qmeCategoryManagement = this;
 
             qmeCategoryManagement.categories;
 
             qmeCategoryManagement.listCategories = function(){
+
+                qmeCategoryService.listCategoryByParent(0)
+                    .then(
+                        function(res){
+                            qmeCategoryManagement.categories = res;
+                        },
+                        function(error){
+                            if(error && error.status && error.status == 403) {
+                                qmeFlashService.Error("Oops.....User not authorized for function, please contact system administrator.");
+
+                            }else {
+                                qmeFlashService.Error("Oops.....Error from service getting category lists, please retry in some time.");
+                            }
+                        }
+                    );
             }
         }
 
