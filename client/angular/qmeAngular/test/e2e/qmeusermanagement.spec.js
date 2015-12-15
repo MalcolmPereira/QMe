bd801b594c8250e5b4ee3bf5e05ba994c8fab320
Malcolm Pereira <malcolm.j.pereira@gmail.com>
2015-12-14 20:22:01 -0500

    updates for test cases

--- client/angular/qmeAngular/test/e2e/qmeusermanagement.spec.js
+++ client/angular/qmeAngular/test/e2e/qmeusermanagement.spec.js
@@ -6,7 +6,7 @@
 
         var EC = protractor.ExpectedConditions;
 
-        it('Perform Login With Valid Admin Credentials, Route to User List and Page through Users', function() {
+        it('Perform Login With Valid Admin Credentials, Route to Admin Core Modules', function() {
 
             browser.get('');
             expect(browser.getTitle()).toEqual('QMe Application');
@@ -64,6 +64,84 @@
 
             element(by.id('userNameMenuId')).click();
             element(by.id('logoutLinkId')).click();
+        });
+
+        it('Perform Login With Valid Admin Credentials, Validate User Module', function() {
+
+            browser.get('');
+            expect(browser.getTitle()).toEqual('QMe Application');
+            expect(element(by.id('qmeAppHeader')).getText()).toEqual('QMe Application');
+            var userEmail = element(by.model('qmeUserCtrl.userEmail'));
+            var userPassword = element(by.model('qmeUserCtrl.userPassword'));
+            expect(userEmail.getText()).toEqual('');
+            expect(userPassword.getText()).toEqual('');
+            expect(element(by.id('emailField')).isDisplayed()).toEqual(true);
+            expect(element(by.id('passwordField')).isDisplayed()).toEqual(true);
+            expect(element(by.id('signInButton')).isDisplayed()).toEqual(true);
+            expect(element(by.id('signInButton')).isEnabled()).toEqual(false);
+            expect(element(by.css('.panel-title')).getText()).toEqual('Home Page');
+
+            userEmail.sendKeys('test.admin@gmail.com');
+            userPassword.sendKeys('testtest');
+            expect(element(by.id('signInButton')).isEnabled()).toEqual(true);
+            element(by.id('signInButton')).click();
+            expect(element(by.id('emailField')).isDisplayed()).toEqual(false);
+            expect(element(by.id('passwordField')).isDisplayed()).toEqual(false);
+            expect(element(by.id('registerButton')).isDisplayed()).toEqual(false);
+            expect(element(by.id('forgotButton')).isDisplayed()).toEqual(false);
+            expect(element(by.id('qmeAppHeader')).getText()).toEqual('QMe Application');
+            expect(element(by.id('loginName')).getText()).toEqual('Test Admin');
+            expect(element(by.id('profileLinkId')).isEnabled()).toBe(true);
+            expect(element(by.id('logoutLinkId')).isEnabled()).toBe(true);
+
+            expect(element(by.id('homeNavLinkId')).isDisplayed()).toEqual(true);
+            expect(element(by.id('homeNavLinkId')).isEnabled()).toEqual(true);
+            expect(element(by.id('usersNavLinkId')).isDisplayed()).toEqual(true);
+            expect(element(by.id('usersNavLinkId')).isEnabled()).toEqual(true);
+            expect(element(by.id('categoriesNavLinkId')).isDisplayed()).toEqual(true);
+            expect(element(by.id('categoriesNavLinkId')).isEnabled()).toEqual(true);
+            expect(element(by.id('questionsNavLinkId')).isDisplayed()).toEqual(true);
+            expect(element(by.id('questionsNavLinkId')).isEnabled()).toEqual(true);
+            expect(element(by.id('quizzesNavLinkId')).isDisplayed()).toEqual(true);
+            expect(element(by.id('quizzesNavLinkId')).isEnabled()).toEqual(true);
+
+            element(by.id('usersNavLinkId')).click();
+            browser.wait(EC.visibilityOf($('#qmepageinationDivId')), 5000);
+            expect(element(by.css('.panel-title')).getText()).toEqual('User List');
+            var headers = element.all(by.css('table.table th')).map(function(elm) {
+                return elm.getText();
+            });
+            expect(headers).toEqual(['User Name', 'User Email', 'First Name', 'Last Name', 'Registered Date', 'Last Login Date', 'User Roles']);
+
+            element(by.id('userId0')).click();
+
+            expect(element(by.id('userEmail')).isDisplayed()).toEqual(true);
+            expect(element(by.id('userEmail')).isEnabled()).toEqual(false);
+            expect(element(by.id('userName')).isDisplayed()).toEqual(true);
+            expect(element(by.id('userName')).isEnabled()).toEqual(false);
+            expect(element(by.id('userPassword')).isDisplayed()).toEqual(true);
+            expect(element(by.id('userPassword')).isEnabled()).toEqual(false);
+            expect(element(by.id('userFirstName')).isDisplayed()).toEqual(true);
+            expect(element(by.id('userFirstName')).isEnabled()).toEqual(true);
+            expect(element(by.id('userLastName')).isDisplayed()).toEqual(true);
+            expect(element(by.id('userLastName')).isEnabled()).toEqual(true);
+            expect(element(by.id('userRoleCheckBoxId')).isDisplayed()).toEqual(true);
+            expect(element(by.id('userRoleCheckBoxId')).isEnabled()).toEqual(false);
+            expect(element(by.id('reviewerRoleCheckBoxId')).isDisplayed()).toEqual(true);
+            expect(element(by.id('reviewerRoleCheckBoxId')).isEnabled()).toEqual(true);
+            expect(element(by.id('moderatorRoleCheckBoxId')).isDisplayed()).toEqual(true);
+            expect(element(by.id('moderatorRoleCheckBoxId')).isEnabled()).toEqual(true);
+            expect(element(by.id('submitUpdateUserButton')).isDisplayed()).toEqual(true);
+            expect(element(by.id('submitUpdateUserButton')).isEnabled()).toEqual(true);
+            expect(element(by.id('cancelButton')).isDisplayed()).toEqual(true);
+            expect(element(by.id('cancelButton')).isEnabled()).toEqual(true);
+            expect(element(by.id('deleteButton')).isDisplayed()).toEqual(true);
+            expect(element(by.id('deleteButton')).isEnabled()).toEqual(true);
+
+            element(by.id('userNameMenuId')).click();
+            element(by.id('logoutLinkId')).click();
+
+
         });
 
 	});
