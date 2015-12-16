3ae391fab2b15dd25e401facd6eb3c925a15b153
Malcolm Pereira <malcolm.j.pereira@gmail.com>
2015-12-15 20:22:52 -0500

    updates for test cases

--- client/angular/qmeAngular/test/e2e/qmeusermanagement.spec.js
+++ client/angular/qmeAngular/test/e2e/qmeusermanagement.spec.js
@@ -187,8 +187,72 @@
             expect(element(by.id('cancelButton')).isDisplayed()).toEqual(true);
             expect(element(by.id('cancelButton')).isEnabled()).toEqual(true);
 
-            element(by.id('userNameMenuId')).click();
-            element(by.id('logoutLinkId')).click();
+            var userEmailAdd = element(by.id('userEmail'));
+            var userNameAdd  = element(by.id('userName'));
+            var userPasswordAdd  = element(by.id('userPassword'));
+            var userPasswordConfirmAdd  = element(by.id('userPasswordConfirm'));
+            var userFirstNameAdd  = element(by.id('userFirstName'));
+            var userLastNameAdd  = element(by.id('userLastName'));
+
+            userEmailAdd.sendKeys('test.me@gmail.com');
+            userNameAdd.sendKeys('testme');
+            userPasswordAdd.sendKeys('testtest');
+            userPasswordConfirmAdd.sendKeys('testtest');
+            userFirstNameAdd.sendKeys('Test');
+            userLastNameAdd.sendKeys('Me');
+
+            expect(element(by.id('submitAddUserButton')).isDisplayed()).toEqual(true);
+            expect(element(by.id('submitAddUserButton')).isEnabled()).toEqual(true);
+            element(by.id('submitAddUserButton')).click();
+            browser.wait(EC.visibilityOf($('#flashMessageId')), 5000);
+
+            expect(element(by.id('flashMessageId')).isDisplayed()).toEqual(true);
+
+            element(by.id('usersNavLinkId')).click();
+            browser.wait(EC.visibilityOf($('#qmepageinationDivId')), 5000);
+            expect(element(by.css('.panel-title')).getText()).toEqual('User List');
+            var headers = element.all(by.css('table.table th')).map(function(elm) {
+                return elm.getText();
+            });
+            expect(headers).toEqual(['User Name', 'User Email', 'First Name', 'Last Name', 'Registered Date', 'Last Login Date', 'User Roles']);
+            expect(element(by.linkText('testme')).isDisplayed()).toBe(true);
+            element(by.linkText('testme')).click();
+
+            expect(element(by.id('userEmail')).isDisplayed()).toEqual(true);
+            expect(element(by.id('userEmail')).isEnabled()).toEqual(false);
+            expect(element(by.id('userName')).isDisplayed()).toEqual(true);
+            expect(element(by.id('userName')).isEnabled()).toEqual(false);
+            expect(element(by.id('userPassword')).isDisplayed()).toEqual(true);
+            expect(element(by.id('userPassword')).isEnabled()).toEqual(false);
+            expect(element(by.id('userFirstName')).isDisplayed()).toEqual(true);
+            expect(element(by.id('userFirstName')).isEnabled()).toEqual(true);
+            expect(element(by.id('userLastName')).isDisplayed()).toEqual(true);
+            expect(element(by.id('userLastName')).isEnabled()).toEqual(true);
+            expect(element(by.id('userRoleCheckBoxId')).isDisplayed()).toEqual(true);
+            expect(element(by.id('userRoleCheckBoxId')).isEnabled()).toEqual(false);
+            expect(element(by.id('reviewerRoleCheckBoxId')).isDisplayed()).toEqual(true);
+            expect(element(by.id('reviewerRoleCheckBoxId')).isEnabled()).toEqual(true);
+            expect(element(by.id('moderatorRoleCheckBoxId')).isDisplayed()).toEqual(true);
+            expect(element(by.id('moderatorRoleCheckBoxId')).isEnabled()).toEqual(true);
+            expect(element(by.id('submitUpdateUserButton')).isDisplayed()).toEqual(true);
+            expect(element(by.id('submitUpdateUserButton')).isEnabled()).toEqual(true);
+            expect(element(by.id('cancelButton')).isDisplayed()).toEqual(true);
+            expect(element(by.id('cancelButton')).isEnabled()).toEqual(true);
+            expect(element(by.id('deleteButton')).isDisplayed()).toEqual(true);
+            expect(element(by.id('deleteButton')).isEnabled()).toEqual(true);
+
+            element(by.id('deleteButton')).click();
+            browser.wait(EC.visibilityOf($('#qmeCofirmModalLabel')), 5000);
+            expect($('.modal-dialog').element(by.id('qmeCofirmModalLabel')).isDisplayed()).toEqual(true);
+            expect($('.modal-dialog').element(by.id('confirmCancelID')).isDisplayed()).toEqual(true);
+            expect($('.modal-dialog').element(by.id('confirmSubmitID')).isDisplayed()).toEqual(true);
+            $('.modal-dialog').element(by.id('confirmSubmitID')).click();
+
+            browser.wait(EC.visibilityOf($('#flashMessageId')), 5000);
+            expect(element(by.id('flashMessageId')).isDisplayed()).toEqual(true);
+
+            browser.wait(EC.visibilityOf($('#qmepageinationDivId')), 5000);
+            expect(element(by.css('.panel-title')).getText()).toEqual('User List');
 
         });
 
