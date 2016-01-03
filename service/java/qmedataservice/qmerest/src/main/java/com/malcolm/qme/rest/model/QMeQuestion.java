e0c536343dc6f3d971b69df5f57c3c08ffdb442e
Malcolm Pereira <malcolm.j.pereira@gmail.com>
2016-01-02 22:27:58 -0500

    Updates for Question Service

---
+++ service/java/qmedataservice/qmerest/src/main/java/com/malcolm/qme/rest/model/QMeQuestion.java
@@ -0,0 +1,15 @@
+/**
+ * Name      : com.malcolm.qme.rest.model.QMeQuestion.java
+ * Date      : 1/2/16
+ * Developer : Malcolm
+ * Purpose   : QMeQuestion Model for REST Service Controller
+ */
+package com.malcolm.qme.rest.model;
+
+/**
+ * QMeQuestion Model for REST Service Controller
+ *
+ * @author Malcolm
+ */
+public class QMeQuestion extends QMeResource {
+}
---
+++ service/java/qmedataservice/qmerest/src/main/java/com/malcolm/qme/rest/model/QMeQuestionDetail.java
@@ -0,0 +1,15 @@
+/**
+ * Name      : com.malcolm.qme.rest.model.QMeQuestionDetail.java
+ * Date      : 1/2/16
+ * Developer : Malcolm
+ * Purpose   : QMeQuestionDetail Model for REST Service Controller
+ */
+package com.malcolm.qme.rest.model;
+
+/**
+ * QMeQuestionDetail Model for REST Service Controller
+ *
+ * @author Malcolm
+ */
+public class QMeQuestionDetail extends QMeQuestion {
+}
