e5fa0502f1851d0d8d30b6ae20172056466087b2
Malcolm Pereira <malcolm.j.pereira@gmail.com>
2016-04-11 23:04:16 -0400

    updates for using gulp. More work needed for gulp tasks

--- client/angular/qmeAngular/gulpfile.js
+++ client/angular/qmeAngular/gulpfile.js
@@ -1,7 +1,27 @@
-var gulp = require('gulp');
+(function () {
 
-gulp.task('default', function() {
+    'use strict';
 
-    console.log("gulp is there now what!!!!");
+    var browserify = require('browserify');
+    var gulp = require('gulp');
+    var source = require('vinyl-source-stream');
+    var buffer = require('vinyl-buffer');
+    var uglify = require('gulp-uglify');
+    var sourcemaps = require('gulp-sourcemaps');
+    var gutil = require('gulp-util');
 
-});
+    var gulp = require('gulp');
+
+    gulp.task('javascript', function() {
+
+        //TODO: Browserify JavaScripts Modules!!!!
+
+    });
+
+    gulp.task('css', function() {
+
+        //TODO: Browserify CSS Modules!!!!
+
+    });
+
+})();
