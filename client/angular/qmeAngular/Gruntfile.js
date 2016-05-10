39488fb94bb6ccc333e959f028efaa6609a1d30a
Malcolm Pereira <malcolm.j.pereira@gmail.com>
2016-05-09 21:58:30 -0400

    Updates for gruntfile

--- client/angular/qmeAngular/Gruntfile.js
+++ client/angular/qmeAngular/Gruntfile.js
@@ -13,6 +13,8 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
 
     grunt.registerTask('default', ['jshint']);
 };
